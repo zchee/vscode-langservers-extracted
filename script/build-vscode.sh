ec807cccdb280e721ca05e3e77ef070d91f7d18a
Koichi Shiraishi <zchee.io@gmail.com>
2025-08-08 15:55:10 +0900

    WIP
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- script/build-vscode.sh
+++ script/build-vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # prepare
 mkdir -p "${DIR}/../tmp"
@@ -8,7 +8,10 @@ mkdir -p "${DIR}/../dist"
 
 # clone
 cd "${DIR}/../tmp" || exit
-git clone --depth=1 git@github.com:microsoft/vscode vscode
+if test -d "${DIR}/../tmp/vscode"; then
+  rm -rf "${DIR}/../tmp/vscode"
+fi
+git clone --depth=1 git@github.com:microsoft/vscode "${DIR}/../tmp/vscode"
 
 # pull
 cd "${DIR}/../tmp/vscode" || exit
@@ -26,16 +29,16 @@ cd "${DIR}/.." || exit
 
 mkdir -p ./dist/css-language-server
 cp -r ./tmp/vscode/extensions/css-language-features/server/out/* ./dist/css-language-server/
-npx -y babel ./dist/css-language-server --out-dir ./lib/css-language-server/
+npx babel ./dist/css-language-server --out-dir ./lib/css-language-server/
 
 mkdir -p ./dist/html-language-server
 cp -r ./tmp/vscode/extensions/html-language-features/server/out/* ./dist/html-language-server/
-npx -y babel ./dist/html-language-server --out-dir ./lib/html-language-server/
+npx babel ./dist/html-language-server --out-dir ./lib/html-language-server/
 
 mkdir -p ./dist/json-language-server
 cp -r ./tmp/vscode/extensions/json-language-features/server/out/* ./dist/json-language-server/
-npx -y babel ./dist/json-language-server --out-dir ./lib/json-language-server/
+npx babel ./dist/json-language-server --out-dir ./lib/json-language-server/
 
 mkdir -p ./dist/markdown-language-server
 cp -r ./tmp/vscode/extensions/markdown-language-features/server/out/* ./dist/markdown-language-server/
-npx -y babel ./dist/markdown-language-server --out-dir ./lib/markdown-language-server/
+npx babel ./dist/markdown-language-server --out-dir ./lib/markdown-language-server/
